c381767f93402a61968e40646537277f899d7a01
NewChakrit <imnew108@gmail.com>
2022-04-05 08:11:01 -0700

    'text-to-json'

--- test/router-parser.test.js
+++ test/router-parser.test.js
@@ -1,72 +1,72 @@
 import { describe, test, expect } from "vitest";
-import RouterParser from "../lib/router-parser";
+import RouterSim from "../lib/router-sim";
 import fs from "fs";
 
 const text = fs.readFileSync("./test/out.txt", "utf-8");
-
-const version = {
-  key: 2.7,
+const text2 = fs.readFileSync("./test/out2.txt", "utf-8");
+const protocol = {
+  Version: "2.7",
 };
 
 const device = {
-  devicePresent: true,
-  modelName: "Blackmagic Smart Videohub 20 x 20",
-  friendlyName: "Smart Videohub 20 x 20",
-  uniqueID: "7C2E0D08FC97",
-  videoInputs: 20,
-  videoProcessingUnits: 0,
-  videoOutputs: 20,
-  videoMonitoringOutputs: 0,
-  serialPorts: 0,
+  "Device present": "true",
+  "Model name": "Blackmagic Smart Videohub 20 x 20",
+  "Friendly name": "Smart Videohub 20 x 20",
+  "Unique ID": "7C2E0D08FC97",
+  "Video inputs": "20",
+  "Video processing units": "0",
+  "Video outputs": "20",
+  "Video monitoring outputs": "0",
+  "Serial ports": "0",
 };
 
 const inputLabels = {
-  0: "1",
-  1: "2",
-  2: "3",
-  3: "4",
-  4: "5",
-  5: "6",
-  6: "7",
-  7: "8",
-  8: "9",
-  9: "10",
-  10: "11",
-  11: "12",
-  12: "13",
-  13: "14",
-  14: "15",
-  15: "16",
-  16: "17",
-  17: "18",
-  18: "19",
-  19: "20",
+  0: "Input 1",
+  1: "Input 2",
+  2: "Input 3",
+  3: "Input 4",
+  4: "Input 5",
+  5: "Input 6",
+  6: "Input 7",
+  7: "Input 8",
+  8: "Input 9",
+  9: "Input 10",
+  10: "Input 11",
+  11: "Input 12",
+  12: "Input 13",
+  13: "Input 14",
+  14: "Input 15",
+  15: "Input 16",
+  16: "Input 17",
+  17: "Input 18",
+  18: "Input 19",
+  19: "Input 20",
 };
 
-const outputLables = {
-  0: "1",
-  1: "2",
-  2: "3",
-  3: "4",
-  4: "5",
-  5: "6",
-  6: "7",
-  7: "8",
-  8: "9",
-  9: "10",
-  10: "11",
-  11: "12",
-  12: "13",
-  13: "14",
-  14: "15",
-  15: "16",
-  16: "17",
-  17: "18",
-  18: "19",
-  19: "20",
+const outputLabels = {
+  0: "Output 1",
+  1: "Output 2",
+  2: "Output 3",
+  3: "Output 4",
+  4: "Output 5",
+  5: "Output 6",
+  6: "Output 7",
+  7: "Output 8",
+  8: "Output 9",
+  9: "Output 10",
+  10: "Output 11",
+  11: "Output 12",
+  12: "Output 13",
+  13: "Output 14",
+  14: "Output 15",
+  15: "Output 16",
+  16: "Output 17",
+  17: "Output 18",
+  18: "Output 19",
+  19: "Output 20",
 };
 
-const routing = {
+const videoOutputRouting = {
   0: "0",
   1: "1",
   2: "2",
@@ -89,15 +89,29 @@ const routing = {
   19: "19",
 };
 
-describe("router-parser", () => {
-  test("test01", () => {
-    let output = RouterParser.parse(text);
+const obj = {
+  protocol,
+  device,
+  inputLabels,
+  outputLabels,
+  videoOutputRouting,
+};
+
+describe("router-sim", () => {
+  test("fromString", () => {
+    let output = RouterSim.fromString(text);
+    // fs.writeFileSync("./data.json", JSON.stringify(output, null, 2));
     expect(output).toBeTypeOf("object");
     // expect(output.device).toBeTypeOf("object");
-    expect(output.version).toEqual(version);
+    expect(output.protocol).toEqual(protocol);
     expect(output.device).toEqual(device);
     expect(output.inputLabels).toEqual(inputLabels);
-    expect(output.outputLables).toEqual(outputLables);
-    expect(output.routing).toEqual(routing);
+    expect(output.outputLabels).toEqual(outputLabels);
+    expect(output.videoOutputRouting).toEqual(videoOutputRouting);
+  });
+  test("toString", () => {
+    let output = RouterSim.toString(obj);
+    // fs.writeFileSync("./test/out2.txt", output);
+    expect(output).toEqual(text2);
   });
 });
